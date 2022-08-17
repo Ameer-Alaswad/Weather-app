a556430164174e1c3e99a9043e4d5b0579705dd0
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-08-17 19:00:04 +0200

    adds react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Form from "./components/form/Form";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
-        <Form />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Form />} />
+          </Routes>
+        </Router>
       </QueryClientProvider>
     </div>
   );
