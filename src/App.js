312f94354fb3df3db4cc94b7e1d102a3a3fd6f77
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-09-21 10:55:16 +0200

    fine tuning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Form from "./components/form/Form";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import DisplayCityWeather from "./components/display-city-weather/DisplayCityWeather";
 import ButtonAppBar from "./components/header/header";
 import SimpleBottomNavigation from "./components/footer/footer";
